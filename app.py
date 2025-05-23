57b0670aa6d2cdddebb5fb918abd37d54e5af7e1
Enochteo <enochowoade@gmail.com>
2025-05-23 18:43:13 -0500

    add play against computer mode

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import random
 plays = [["","",""], ["","",""], ["","",""]]
 def board(plays):
     for i in range(len(plays)):
@@ -33,25 +34,72 @@ def tie_checker():
             return False
     return not check_winner()
 
+def multiplayer_game():
+    turn = 0
+    while True:
+        print(board(plays))
+        player = "X" if turn % 2 == 0 else "O"
+        try:
+            position = int(input(f"Player {player} Choose your position (0-8): "))
+        except ValueError:
+            print("Please enter a number from 0 to 8.")
+            continue
+        if play(position, player):
+            turn += 1
+        if check_winner():
+            print(board(plays))
+            print(f"{player} won !!!")
+            break
+        if tie_checker():
+            print(board(plays))
+            print("Its a tie!")
+            break
 
-turn = 0
-while True:
-    print(board(plays))
-    player = "X" if turn % 2 == 0 else "O"
-    try:
-        position = int(input(f"Player {player} Choose your position (0-8): "))
-    except ValueError:
-        print("Please enter a number from 0 to 8.")
-        continue
-    if play(position, player):
+def play_against_computer():
+    turn = 0
+    while True:
+        try:
+            position = int(input(f"Player X, Choose your position (0-8): "))
+        except ValueError:
+            print("Please enter a number from 0 to 8")
+            continue
+        if not play(position, "X"):
+            print("That spot is taken")
+            continue
         turn += 1
-    if check_winner():
-        print(board(plays))
-        print(f"{player} won !!!")
-        break
-    if tie_checker():
-        print(board(plays))
-        print("Its a tie!")
-        break
+        if check_winner():
+            print(board(plays))
+            print("You won !!!")
+            break
+        if tie_checker():
+            print(board(plays))
+            print("Its a tie!")
+            break
+
+        empty_positions = [i for i in range(9) if plays[i // 3][i % 3] == ""]
+        if empty_positions:
+            position = random.choice(empty_positions)
+            play(position, "O")
+            print(board(plays))
+            print(f"Computer chose position{position}")
+            turn += 1
+        if check_winner():
+            print(board(plays))
+            print("Computer won !!!")
+            break
+        if tie_checker():
+            print(board(plays))
+            print("Its a tie!")
+            break
+        
+
+
+mode = input("Play against Player (1) or Computer(2)?")
+if mode == "1":
+    multiplayer_game()
+elif mode == "2":
+    play_against_computer()
+else:
+    print("We don't have such mode yet")
 
 
